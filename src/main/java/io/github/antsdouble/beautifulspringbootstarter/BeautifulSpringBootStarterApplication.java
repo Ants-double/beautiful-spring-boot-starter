2769ac0a9da779f4a60f44b4a6d13da0f939db51
lixin <liyangyang0808@126.com>
2022-12-04 08:29:41 +0800

    init starter

---
+++ src/main/java/io/github/antsdouble/beautifulspringbootstarter/BeautifulSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.antsdouble.beautifulspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/antsdouble/beautifulspringbootstarter/BeautifulSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.antsdouble.beautifulspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
